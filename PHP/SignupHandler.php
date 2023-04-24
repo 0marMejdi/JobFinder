d4f700a62052c92122cd8ad8e7d73456e7cac101
mohamedaziz <azizbalti50@gmail.com>
2023-04-24 17:28:11 +0100

    "Added testing on existing email and error handling "

--- PHP/SignupHandler.php
+++ PHP/SignupHandler.php
@@ -6,7 +6,12 @@ $LastName= $_POST['LastName'];
 $Email= $_POST['Email'];
 $Password= $_POST['Password'];
 $Date=$_POST['Date'];
-CRUDUSERS::create($FirstName,$LastName,$Email,$Password,$Date);
-setcookie('aziz',true,time()+60*3);
-header("Location: ../index.php");
+if (CRUDUSERS::readOne($Email)>0) {
+    header("Location: ../register.php?error=1");
+}
+else {
+    CRUDUSERS::create($FirstName, $LastName, $Email, $Password, $Date);
+//setcookie('aziz',true,time()+60*3);
+    header("Location: ../index.php");
+}
 ?>
