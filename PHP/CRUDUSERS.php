4d596fb964687054f0fa61ef0cdcca167dc098e7
mohamedaziz <azizbalti50@gmail.com>
2023-04-24 17:28:41 +0100

    "Adding some crud functions to check by email , will add more later "

--- PHP/CRUDUSERS.php
+++ PHP/CRUDUSERS.php
@@ -24,61 +24,35 @@ class CRUDUSERS
             echo "FirstName= {$key->FirstName} , LastName= {$key->LastName} , Email= {$key->Email} , Password= {$key->Password} , DateOfBirth= {$key->DateOfBirth} <br>";
         }
     }
-    public static function readById($id)
+    // create all crud functions for the table users FirstName,LastName,Email,Password,DateOfBirth with prepare statements
+public static function update($firstname, $lastname, $email, $password, $date)
     {
-        echo "Read the data from the table aziz by id:{$id} <br>";
         $bdd = ConnexionBD::getInstance();
-        //$requete="SELECT * FROM aziz WHERE id=$id";
-        $requete=$bdd->prepare("SELECT * FROM users WHERE id=:id");
-        $requete->excute(array('id'=>$id));
-        $obj= $requete->fetchAll(PDO::FETCH_OBJ);
-        foreach ($obj as $key) {
-            echo "id= {$key->id} name= {$key->name} surname= {$key->surname} address= {$key->address} <br>";
+        $requete = "UPDATE `users` SET `FirstName`='{$firstname}',`LastName`='{$lastname}',`Email`='{$email}',`Password`='{$password}',`DateOfBirth`='{$date}' WHERE `Email`='{$email}'";
+        $reponse = $bdd->prepare($requete);
+        $reponse->execute();
+        echo "Updated successfully! <br>";
+    }
+    public static function delete($email)
+    {
+        $bdd = ConnexionBD::getInstance();
+        $requete = "DELETE FROM `users` WHERE `Email`='{$email}'";
+        $reponse = $bdd->prepare($requete);
+        $reponse->execute();
+        echo "Deleted successfully! <br>";
+    }
+    public static function readOne($email)
+    {
+        echo "Read the data from the table users <br>";
+        $bdd = ConnexionBD::getInstance();
+        $requete = "SELECT * FROM users WHERE `Email`='{$email}'";
+        $reponse = $bdd->prepare($requete);
+        $reponse->execute();
+        $obj = $reponse->fetchAll(PDO::FETCH_OBJ);
+        /*foreach ($obj as $key) {
+            echo "FirstName= {$key->FirstName} , LastName= {$key->LastName} , Email= {$key->Email} , Password= {$key->Password} , DateOfBirth= {$key->DateOfBirth} <br>";
         }
-    }
-    //make a function to read by name like the code above using the prepare,excute functions
-    public static function readByName($name)
-    {
-        echo "Read the data from the table aziz by name:{$name} <br>";
-        $bdd = ConnexionBD::getInstance();
-        $requete=$bdd->prepare("SELECT * FROM aziz WHERE name=:name");
-        $requete->excute(array('name'=>$name));
-        $obj= $requete->fetchAll(PDO::FETCH_OBJ);
-        foreach ($obj as $key) {
-            echo "id= {$key->id} name= {$key->name} surname= {$key->surname} address= {$key->address} <br>";
-        }    }
-    public static function readBySurname($surname)
-    {
-        echo "Read the data from the table aziz by surname:{$surname} <br>";
-        $bdd = ConnexionBD::getInstance();
-        $requete=$bdd->prepare("SELECT * FROM aziz WHERE surname=:surname");
-        $requete->excute(array('surname'=>$surname));
-        $obj= $requete->fetchAll(PDO::FETCH_OBJ);
-        foreach ($obj as $key) {
-            echo "id= {$key->id} name= {$key->name} surname= {$key->surname} address= {$key->address} <br>";
-        }    }
-    public static function readByAddress($address)
-    {
-        echo "Read the data from the table aziz by address:{$address} <br>";
-        $bdd = ConnexionBD::getInstance();
-        $requete=$bdd->prepare("SELECT * FROM aziz WHERE address=:address");
-        $requete->excute(array('address'=>$address));
-        $obj= $requete->fetchAll(PDO::FETCH_OBJ);
-        foreach ($obj as $key) {
-            echo "id= {$key->id} name= {$key->name} surname= {$key->surname} address= {$key->address} <br>";
-        }    }
-    public static function  updateById($id,$name,$surname,$address)
-    {
-        $bdd = ConnexionBD::getInstance();
-        $requete=$bdd->prepare("UPDATE aziz SET name=:name,surname=:surname,address=:address WHERE id=:id");
-        $requete->excute(array('id'=>$id,'name'=>$name,'surname'=>$surname,'address'=>$address));
-        echo "Update successfully! <br>";
-    }
-    public static function deleteById($id)
-    {
-        $bdd = ConnexionBD::getInstance();
-        $requete=$bdd->prepare("DELETE FROM aziz WHERE id=:id");
-        $requete->execute(array('id'=>$id));
-        echo "Delete successfully! <br>";
+        */
+        return $reponse->rowCount();
     }
 }
