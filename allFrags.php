217fcf9eb7cfa382b096e8265c05801246a53ab0
Balti Mohamed Aziz <mohamedaziz.balti@insat.ucar.tn>
2023-05-05 02:46:03 +0100

    "minor changes"

--- allFrags.php
+++ allFrags.php
@@ -6,11 +6,9 @@ include_once "frags/errorManagement.php";
 include_once "frags/filesHandling.php";
 
 require_once("autoload.php");
-// include_once "frags/classes/Employer.php";
-// include_once "frags/classes/EmployerRepository.php";
 // include_once "frags/classes/ConnexionBD.php";
 // include_once "frags/classes/CRUDUSERS.php";
 // include_once "frags/classes/ObjectRepository.php";
 // include_once "frags/classes/Person.php";
-// include_once "frags/classes/UserRepository.php";
-// include_once "frags/classes/User.php";
+// include_once "frags/classes/JobSeekerRepository.php";
+// include_once "frags/classes/JobSeeker.php";
