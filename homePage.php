eab34f81b6d74c8bc353f5f497f94834dd55188b
Balti Mohamed Aziz <mohamedaziz.balti@insat.ucar.tn>
2023-05-05 02:42:13 +0100

    "testing current user information on homePage.php"

--- homePage.php
+++ homePage.php
@@ -1,17 +1,16 @@
 <?php
 include_once "allFrags.php";
 includeHeader("Home");
-needsAuthentication();
+//needsAuthentication();
+session_start();
 $detail = $_SESSION["currentUser"];
-$str = "name: $detail->name   <br> Last Name : $detail->lastName <br>";
-$str .= "Password: $detail->password   <br>  E-mail : $detail->email";
+echo $detail;
 ?>
 
 <body>
     <div class="container">
         <div class="alert alert-info"> Welcome to HomePage</div>
 
-        <?=$str?>
         <form action="disconnect.php" method="post">
             <button class="w-100 btn btn-light btn btn-outline-success btn-success" type="submit">Disconnect</button>
         </form>
