ae37d43f41513c4e3cf75b29f606dcbe845ccf90
Balti Mohamed Aziz <mohamedaziz.balti@insat.ucar.tn>
2023-05-07 22:18:27 +0100

    "added attributes and functions"

--- frags/classes/JobOffer.php
+++ frags/classes/JobOffer.php
@@ -9,8 +9,79 @@ class JobOffer
     public string $publishDate;
     public string $contractType;
     public string $workTime;
-//    public string
+    public string $workType;
+    public string $location;
+    public string $education;
+    public string $experience;
+    public float $salary;
+//make constructor
+    public function __construct(
+        $title="",
+        $description="",
+        $workTime="",
+        $workType="",
+        $contractType="",
+        $location="",
+        $education="",
+        $experience="",
+        $salary=0.0)
+    {
+        $this->id = $this->gen_uuid();
+        $this->title = $title;
+        $this->description = $description;
+        $this->contractType = $contractType;
+        $this->workTime = $workTime;
+        $this->workType = $workType;
+        $this->location = $location;
+        $this->education = $education;
+        $this->experience = $experience;
+        $this->salary = $salary;
+        $this->companyEmail = $_SESSION["currentUser"]->email;
+        $this->publishDate = date("Y-m-d");
+    }
 
+    function gen_uuid()
+    {
+        $uuid = array(
+            'time_low' => 0,
+            'time_mid' => 0,
+            'time_hi' => 0,
+            'clock_seq_hi' => 0,
+            'clock_seq_low' => 0,
+            'node' => array()
+        );
 
+        $uuid['time_low'] = mt_rand(0, 0xffff) + (mt_rand(0, 0xffff) << 16);
+        $uuid['time_mid'] = mt_rand(0, 0xffff);
+        $uuid['time_hi'] = (4 << 12) | (mt_rand(0, 0x1000));
+        $uuid['clock_seq_hi'] = (1 << 7) | (mt_rand(0, 128));
+        $uuid['clock_seq_low'] = mt_rand(0, 255);
 
+        for ($i = 0; $i < 6; $i++) {
+            $uuid['node'][$i] = mt_rand(0, 255);
+        }
+
+        $uuid = sprintf('%08x-%04x-%04x-%02x%02x-%04x%02x%02x%02x%02x%02x',
+            $uuid['time_low'],
+            $uuid['time_mid'],
+            $uuid['time_hi'],
+            $uuid['clock_seq_hi'],
+            $uuid['clock_seq_low'],
+            $uuid['node'][0],
+            $uuid['node'][1],
+            $uuid['node'][2],
+            $uuid['node'][3],
+            $uuid['node'][4],
+            $uuid['node'][5]
+        );
+
+        return $uuid;
+    }
+
+    public function __toString(): string
+    {
+        //make \n between each attribute
+        return "id : {$this->id}" . "<br>" . "title : {$this->title}" . "<br>" . "description : {$this->description}" . "<br>" . "publishDate : {$this->publishDate}" . "<br>" . "contractType : {$this->contractType}" . "<br>" . "workTime : {$this->workTime}" . "<br>" . "workType : {$this->workType}" . "<br>" . "location : {$this->location}" . "<br>" . "education : {$this->education}" . "<br>" . "experience : {$this->experience}" . "<br>" . "salary : {$this->salary}" . "<br>" . "companyEmail : {$this->companyEmail}" . "<br>
+    ";
+    }
 }
