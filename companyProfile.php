42163e168cf2ed5b8c7ee208e61b09e2137f1ad4
ahmedsilinii <ahmedsilini5@gmail.com>
2023-05-08 20:17:14 +0100

    Create companyProfile.php

---
+++ companyProfile.php
@@ -0,0 +1,135 @@
+<!-- Nitfehmou chnouwa bich n7otou fih -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>My Profile</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- logo -->
+    <link href="assets\img\logo.png" rel="icon">
+
+    <!-- CSS -->
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+    <body>
+
+        <!-- ======= Header ======= -->
+        <header id="header" class="fixed-top d-flex align-items-center">
+            <div class="container d-flex align-items-center justify-content-between">
+
+                <div class="logo">
+                    <h1><a href="companyProfile.php">JobFinder</a></h1>
+                </div>
+
+                <nav id="navbar" class="navbar">
+
+                    <ul>
+                        <li><a class="nav-link scrollto" href="">Home</a></li>
+                        <li><a class="nav-link scrollto active" href="companyProfile.php">My Profile</a></li>
+                        <li><a class="nav-link scrollto" href="">My Job Offers</a></li>
+                        <li><a class="login " href="login.php">Disconnect</a></li>
+                    </ul>
+                    <i class="bi bi-list mobile-nav-toggle"></i>
+                </nav><!-- .navbar -->
+
+            </div>
+        </header><!-- End Header -->
+
+
+
+        <br><br>
+        <!-- Company Profile Section -->
+        <section id="company-profile" class="company-profile">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="company-img" data-aos="fade-right" data-aos-delay="100">
+                            <img src="assets\templates\default-company.jpg" alt="Company Logo">
+                        </div>
+                    </div>
+                    <div class="col-lg-8 mt-5 mt-lg-0">
+                        <div class="company-info" data-aos="fade-left" data-aos-delay="200">
+                            <h3>XYZ Company</h3>
+                            <p><i class="bx bx-envelope"></i> info@xyzcompany.com</p>
+                            <div class="description">
+                                <h5>Description</h5>
+                                <p>XYZ Company is a leading provider of innovative solutions in the technology industry.
+                                </p>
+                            </div>
+                            <div class="sector">
+                                <h5>Sector</h5>
+                                <ul>
+                                    <li>Tech</li>
+                                </ul>
+                            </div>
+                            <div class="subsector">
+                                <h5>Subsector</h5>
+                                <ul>
+                                    <li>Big Data</li>
+                                </ul>
+                            </div>
+                            <div class="size">
+                                <h5>Size</h5>
+                                <p>500 employees</p>
+                            </div>
+                            <div class="founded">
+                                <h5>Founded</h5>
+                                <p>2005</p>
+                            </div>
+                            <p><i class="bx bx-phone-call"></i> +1 123 456 7890</p>
+                            <p><i class="bx bx-globe"></i> Country: USA</p>
+                            <p><i class="bx bx-map"></i> Region: California</p>
+                            <p><i class="bx bx-map"></i> Address: 123 Main St, San Francisco, CA 94111</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <!-- ======= Footer ======= -->
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <p class="text-center">
+                            Contact Us: Phone: +216 92 220 708 | Email: info@jobfinder.com | Address: Centre Urbain Nord
+                            BP
+                            676 - 1080
+                            Tunis
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+
+
+        <!-- End Footer -->
+
+
+
+        <!-- JS -->
+        <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+        <script src="assets/vendor/aos/aos.js"></script>
+        <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+        <script src="assets/js/main.js"></script>
+
+    </body>
+
+</html>
